ae4f1ab53fb1d25239cef3c58f27e5fe4dd9386a
a239441 <vinod.sharma@syneoshealth.com>
2022-02-17 14:58:19 +0530

    Added home controller

---
+++ API1/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
